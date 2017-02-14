d5f48aa171a65d74c79432a4e04d4f1f39215a51
daxapps <daxapps777@gmail.com>
2017-02-14 15:41:38 -0600

    Create custom tableViewCell

--- MySocialApp/FeedVC.swift
+++ MySocialApp/FeedVC.swift
@@ -10,17 +10,43 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3 //posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+//        let post = posts[indexPath.row]
+//        
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+//            
+//            if let img = FeedVC.imageCache.object(forKey: post.imageUrl as NSString) {
+//                cell.configureCell(post: post, img: img)
+//            } else {
+//                cell.configureCell(post: post)
+//            }
+//            return cell
+//        } else {
+//            return PostCell()
+//        }
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
 
     @IBAction func signOutTapped(_ sender: Any) {
---
+++ MySocialApp/PostCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostCell.swift
+//  MySocialApp
+//
+//  Created by Jason Crawford on 2/14/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    @IBOutlet weak var likeImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
