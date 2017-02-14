5fbbc7cc53ba8834e0c218747d09707a1610f777
daxapps <daxapps777@gmail.com>
2017-02-14 06:55:41 -0600

    Add Email Authentication
    - unable to test on my iPhone b/c no hideKeyboard function
    - authenticates on simulator

--- MySocialApp/SignInVC.swift
+++ MySocialApp/SignInVC.swift
@@ -10,10 +10,13 @@ import UIKit
 import FBSDKLoginKit
 //import FBSDKCoreKit
 import Firebase
-//import SwiftKeychainWrapper
+import SwiftKeychainWrapper
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -55,6 +58,32 @@ class SignInVC: UIViewController {
         })
     }
     
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("Dax: Email user authenticated with Firebase")
+//                    if let user = user {
+//                        let userData = ["provider": user.providerID]
+//                        self.completeSignIn(id: user.uid, userData: userData)
+//                    }
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("Dax: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("Dax: Successfully authenticated with Firebase")
+//                            if let user = user {
+//                                let userData = ["provider": user.providerID]
+//                                self.completeSignIn(id: user.uid, userData: userData)
+//                            }
+                        }
+                    })
+                }
+            })
+        }
+    }
     
 //    func completeSignIn(id: String, userData: Dictionary<String, String>) {
 //        DataService.ds.createFirbaseDBUser(uid: id, userData: userData)
@@ -63,8 +92,6 @@ class SignInVC: UIViewController {
 //        print("Dax: Data saved to keychain \(keychainResult)")
 //        performSegue(withIdentifier: "goToFeed", sender: nil)
 //    }
-
-    
     
     
     
