08c6e675586646ecb2a5d509ebe58d20995e0581
daxapps <daxapps777@gmail.com>
2017-02-15 17:30:47 -0600

    Make social media post

--- MySocialApp/DataService.swift
+++ MySocialApp/DataService.swift
@@ -37,14 +37,14 @@ class DataService {
         return _REF_USERS
     }
     
-//    var REF_USER_CURRENT: FIRDatabaseReference {
-//        //let uid = KeychainWrapper.stringForKey(KEY_UID)
-//        //let uid = KeychainWrapper.set(KEY_UID)
-//        //let uid = KeychainWrapper.defaultKeychainWrapper.string(forKey: KEY_UID)
-//        let uid = KeychainWrapper.standard.string(forKey: KEY_UID)
-//        let user = REF_USERS.child(uid!)
-//        return user
-//    }
+    var REF_USER_CURRENT: FIRDatabaseReference {
+        //let uid = KeychainWrapper.stringForKey(KEY_UID)
+        //let uid = KeychainWrapper.set(KEY_UID)
+        //let uid = KeychainWrapper.defaultKeychainWrapper.string(forKey: KEY_UID)
+        let uid = KeychainWrapper.standard.string(forKey: KEY_UID)
+        let user = REF_USERS.child(uid!)
+        return user
+    }
     
     var REF_POST_IMAGES: FIRStorageReference {
         return _REF_POST_IMAGES
