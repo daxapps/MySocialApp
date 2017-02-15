9bfb8e180fa8322104da6c6dfd1f7bcca27ee98c
daxapps <daxapps777@gmail.com>
2017-02-15 08:06:03 -0600

    UIPickerView post to Firebase

--- MySocialApp/FeedVC.swift
+++ MySocialApp/FeedVC.swift
@@ -10,17 +10,25 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     var posts = [Post]()
+    var imagePicker: UIImagePickerController!
+    //static var imageCache: Cache<NSString, UIImage> = Cache()
+    var imageSelected = false
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageAdd: CircleView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             //print(snapshot.value ?? "??")
@@ -67,6 +75,50 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         }
         
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            imageAdd.image = image
+            //imageSelected = true
+        } else {
+            print("Dax: A valid image wasn't selected")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addImageTapped(_ sender: AnyObject) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+//    @IBAction func postBtnTapped(_ sender: AnyObject) {
+//        guard let caption = captionField.text, caption != "" else {
+//            print("JESS: Caption must be entered")
+//            return
+//        }
+//        guard let img = imageAdd.image, imageSelected == true else {
+//            print("JESS: An image must be selected")
+//            return
+//        }
+//        
+//        if let imgData = UIImageJPEGRepresentation(img, 0.2) {
+//            
+//            let imgUid = NSUUID().uuidString
+//            let metadata = FIRStorageMetadata()
+//            metadata.contentType = "image/jpeg"
+//            
+//            DataService.ds.REF_POST_IMAGES.child(imgUid).put(imgData, metadata: metadata) { (metadata, error) in
+//                if error != nil {
+//                    print("JESS: Unable to upload image to Firebasee torage")
+//                } else {
+//                    print("JESS: Successfully uploaded image to Firebase storage")
+//                    let downloadURL = metadata?.downloadURL()?.absoluteString
+//                    if let url = downloadURL {
+//                        self.postToFirebase(imgUrl: url)
+//                    }
+//                }
+//            }
+//        }
+//    }
 
     @IBAction func signOutTapped(_ sender: Any) {
         
