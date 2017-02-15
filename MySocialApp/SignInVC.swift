cf3b641b83f4d6f7e3852ae9970f56f63e5b0930
daxapps <daxapps777@gmail.com>
2017-02-15 05:56:33 -0600

    Create database user for Firebase

--- MySocialApp/SignInVC.swift
+++ MySocialApp/SignInVC.swift
@@ -88,7 +88,7 @@ class SignInVC: UIViewController {
     }
     
     func completeSignIn(id: String, userData: Dictionary<String, String>) {
-        //DataService.ds.createFirbaseDBUser(uid: id, userData: userData)
+        DataService.ds.createFirbaseDBUser(uid: id, userData: userData)
         //let keychainResult = KeychainWrapper.setString(id, forKey: KEY_UID)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("Dax: Data saved to keychain \(keychainResult)")
